99b64cfa397bc50b00a9f957daf13992712101be
RicchKrixx <gadrijasper1@gmail.com>
2025-07-27 20:19:12 +0000

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,13 +1,12 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyB0BBzQzGX6MHVSjn09VSD-JGpSwOu8EqQ",
   authDomain: "bixmax-6c24c.firebaseapp.com",
   projectId: "bixmax-6c24c",
-  storageBucket: "bixmax-6c24c.appspot.com",
+  storageBucket: "bixmax-6c24c.appspot.com", // ✅ fixed
   messagingSenderId: "37740442541",
   appId: "1:37740442541:web:a2e2b9bb81c6a8e76c6724"
-});
-
+};
 const messaging = firebase.messaging();
